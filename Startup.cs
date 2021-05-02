a0dfa3477fdb21c1dcdd84fd4b14072d6172f5ce
purambharath <puram.bharath@gmail.com>
2021-05-02 08:35:13 -0400

    end of session 3

--- Startup.cs
+++ Startup.cs
@@ -31,15 +31,18 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
 
+             
+
             services.AddDbContext<DataContext>(options => {
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
-            
+           services.AddCors();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,6 +59,8 @@ namespace API
 
             app.UseRouting();
 
+             app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+            //  app.UseCors("ApiCorsPolicy");
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
